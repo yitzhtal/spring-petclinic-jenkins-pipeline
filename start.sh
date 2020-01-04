74183f2e17104cb66898b0f1cc97394fd03c88f3
Tal Yitzhak <talyitzhak100@gmail.com>
2020-01-05 00:56:53 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@ wget -O jenkins_home.zip https://dl.dropboxusercontent.com/s/o13g19ok5zi4ega/jen
 sudo unzip -X -o jenkins_home.zip                                  && \
 rm jenkins_home.zip
 
-sudo chown -R $(whoami) $(find $(pwd)/jenkins_home -maxdepth 0 -type d)
+sudo chown -R "$(whoami)" "$(find $(pwd)/jenkins_home -maxdepth 0 -type d)"
 
 # Run the jenkins-docker container, with mount to jenkins_home; on port 8080
 # Relative path was not used in Mount; therefore find command was used to find
 # the full path of the jenkins_home folder
 docker run --name jenkins-docker \
  -u root -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock \
- -v $(find $(pwd)/jenkins_home -maxdepth 0 -type d):/var/jenkins_home \
+ -v "$(find $(pwd)/jenkins_home -maxdepth 0 -type d)":/var/jenkins_home \
  tyitzhak/jenkins-docker:latest
